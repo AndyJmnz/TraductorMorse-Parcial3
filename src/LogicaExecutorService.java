050ee3c2dc33b91bfc85f93d706c724b4bd70217
Andy Jimenez <a17100657@gmail.com>
2024-06-18 17:40:33 -0600

    tiempos 6

--- src/LogicaExecutorService.java
+++ src/LogicaExecutorService.java
@@ -1,9 +1,22 @@
 import java.util.HashMap;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class LogicaExecutorService {
-    
+
+
+    public static void Time(char[] array) {
+        if (array.length <= 5000) {
+            Random rand = new Random();
+            int sleepTime = 1 + rand.nextInt(3);
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     public static String traducirTextoConExecutorService(char[] charArray, HashMap<Character, String> codigoMorse) {
         StringBuilder morseCode = new StringBuilder();
 
